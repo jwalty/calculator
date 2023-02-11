fa7cc3ec4d798a3e2e5692ee5f019699afe2c0a7
JWalty <therealjwalty@gmail.com>
2023-02-11 10:55:26 -0500

    Add base calculator functionality

---
+++ main.js
@@ -0,0 +1,61 @@
+// TODO:
+// clear better
+// decimal support
+// divide by zero protection
+// disallow strings of numbers to be added (calculate answer after each operation press)
+
+let displayValue = "";
+let currentOperator = "untouched";
+let numberBeforeOperator;
+let numberAfterOperator;
+
+//basic calculator functionality
+function operate(operation, n1, n2) {
+    switch (operation) {
+        case "+":
+            return n1 + n2;
+        case "-":
+            return n1 - n2; 
+        case "/":
+            return n1 / n2;
+        case "*":
+            return n1 * n2;
+    }
+}
+
+//number buttons
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((e) => {
+    e.addEventListener('click', () => {
+        displayValue = displayValue + (e.id);
+        document.getElementById('displayValue').textContent = displayValue;
+    });
+});
+
+//operator buttons
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (e.id === currentOperator) {
+            return;
+        }
+        
+        if (e.id === '=') {
+            numberAfterOperator = parseInt(displayValue);
+            displayValue = operate(currentOperator,numberBeforeOperator,numberAfterOperator);
+            document.getElementById('displayValue').textContent = displayValue;
+            return;
+        }
+        currentOperator = (e.id);
+        numberBeforeOperator = parseInt(displayValue);
+        document.getElementById('history').textContent = `${numberBeforeOperator} ${currentOperator}`;
+        displayValue = "";
+    });
+});
+
+//clear button
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    displayValue = " ";
+    document.getElementById('displayValue').textContent = displayValue;
+});
